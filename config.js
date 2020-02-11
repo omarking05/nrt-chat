69e9783658bf158b6e6eed8c9403e4c954b1204f
Omar Ahmed <omarking05@gmail.com>
2020-02-11 18:02:27 +0200

    add Taras mongodb code

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
 const express     = require('express');
 const httpServer  = require('http');
 const socketIo    = require('socket.io');
+const mongodb     = require('mongodb');
 
 const app         = express();
 const Server      = httpServer.Server(app);
 const io          = socketIo(Server);
+const mongoClient = new mongodb.MongoClient(process.env.MONGO_CONNECTION_STRING, { useUnifiedTopology: true });
 
 module.exports = {
   app,
   Server,
-  io
+  io,
+  mongoClient
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
-const path              = require('path');
-const twilio            = require('twilio');
-const WhatsAppMessage   = require('../models/whatsapp-message');
-const routes            = require('express').Router();
-const { io }            = require('../config');
-const accountId         = process.env.TWILIO_ACCOUNT_ID;
-const authToken         = process.env.TWILIO_AUTH_TOKEN;
-const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
-const client            = twilio(accountId, authToken);
+const path                = require('path');
+const twilio              = require('twilio');
+const WhatsAppMessage     = require('../models/whatsapp-message');
+const routes              = require('express').Router();
+const { io, mongoClient } = require('../config');
+const accountId           = process.env.TWILIO_ACCOUNT_ID;
+const authToken           = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber   = process.env.TWILIO_PHONE_NUMBER;
+const client              = twilio(accountId, authToken);
+let db                    = {};
+
+mongoClient.connect(function(err, client){
+  if(err) return console.log(err);
+  db = client.db('cobrowser_v3');
+  db.createCollection('nrt_messages');
+});
 
 routes.get('/', (_, res) => {
   res.send('Hey There.');
@@ -37,11 +44,26 @@ routes.post('/chat', (req, res) => {
 
   // Twiml response
   const response  = twilio.twiml.MessagingResponse(replyBody);
+
+  saveIncomingMessageToDb(formattedMessage);
+
   res.send(response);
 });
 
 routes.get('/agent', (_, res) => {
   res.sendFile(path.join(__dirname + '/../views/agent.html'));
 });
+
+function saveIncomingMessageToDb(formattedMessage) {
+  const message = {
+    'from'  : formattedMessage.From,
+    'to'    : formattedMessage.To,
+    'body'  : formattedMessage.Body,
+    'status': 'pending',
+    'time'  : new Date()
+  };
+
+  db.collection('nrt_messages').insertOne(message);
+}
 
 module.exports = routes;
