0152951a10bafdbccc6990400144fd9d087b832c
asmaa <asmaa.tahoon25@gmail.com>
2020-02-12 17:45:48 +0200

    move code from routes to controller

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,5 +1,11 @@
+const path                = require('path');
 const Chat                = require('../models/chat');
 const Message             = require('../models/message');
+const chatService         = require('../services/chatService');
+const IncominMessage      = require('../models/whatsapp/incoming-message');
+const { io }              = require('../config');
+const twilio              = require('twilio');
+
 
 module.exports = {
 
@@ -11,5 +17,27 @@ module.exports = {
             return reply.send(error).code(500);
         }
 
+    },
+
+    postChat(req, res) {
+        const rawMessage        = req.body;
+        const formattedMessage  = new IncominMessage(rawMessage);
+
+        // TODO Need to use callback in case when we have troubles with saving into DB
+        chatService.saveIncomingMessageToDb(formattedMessage);
+        io.sockets.emit('wa_message', formattedMessage);
+
+        console.log('--------------------------');
+        console.log(`From: ${formattedMessage.from}`);
+        console.log(`Body: ${formattedMessage.body}`);
+        console.log('--------------------------');
+
+        // Twiml response
+        const response  = twilio.twiml.MessagingResponse('replied');
+        res.send(response);
+    },
+
+    showAgent(req, res) {
+        res.sendFile(path.join(__dirname + '/../views/agent.html'));
     }
 };
